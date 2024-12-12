b5b318996ec33603f0a3c6d6972de23ef6cb8999
Aditya kumar <aditya.kumar1.cs.2022@mitmeerut.ac.in>
2024-12-12 23:37:19 +0530

    Update team member details and replace social media links with email

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -4,69 +4,68 @@ import React from 'react';
 function Team() {
   const teamMembers = [
     {
-      name: 'Rahul',
-      position: 'CEO',
-      image: 'https://tse3.mm.bing.net/th?id=OIP.2TBlFmTmCrCphzvo4qo-gQHaHa&pid=Api&P=0&h=180',
-      instagram: 'https://www.instagram.com',
-      linkedin: 'https://www.linkedin.com',
-      twitter: 'https://www.twitter.com',
+      name: 'Vanshika Saxena',
+      position: 'Co-Ordinator',
+      image: 'https://tse3.mm.bing.net/th?id=OIP.LhGcO_gnxGBIqHMR3D4zCAAAAA&pid=Api&P=0&h=180',
+      email: 'mailto:vanshika.saxena.cs.2022@mitmeerut.ac.in',
+      linkedin: 'https://www.linkedin.com/in/vanshika-saxena-5v?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
     },
     {
-      name: 'Tabish',
-      position: 'CTO',
-      image: 'https://tse3.mm.bing.net/th?id=OIP.2TBlFmTmCrCphzvo4qo-gQHaHa&pid=Api&P=0&h=180',
-      instagram: 'https://www.instagram.com',
-      linkedin: 'https://www.linkedin.com',
-      twitter: 'https://www.twitter.com',
+      name: 'Rahul Kr. Singh',
+      position: 'Head Operator',
+      image: 'https://tse3.mm.bing.net/th?id=OIP.EyoxKwoHzZnbWlF3B5O0EwHaFO&pid=Api&P=0&h=180',
+      email: 'mailto:rahul.singh.ds.2022@mitmeerut.ac.in',
+      linkedin: 'https://linkedin.com/in/rrrahulsingh',
     },
     {
-      name: 'Ajay',
-      position: 'Developer',
-      image: 'https://tse3.mm.bing.net/th?id=OIP.2TBlFmTmCrCphzvo4qo-gQHaHa&pid=Api&P=0&h=180',
-      instagram: 'https://www.instagram.com',
-      linkedin: 'https://www.linkedin.com',
-      twitter: 'https://www.twitter.com',
+      name: 'Md Tabish Manzer',
+      position: 'Social Media Head',
+      image: 'https://tse4.mm.bing.net/th?id=OIP.nDdVGtbkY-1CS_BBGPaIKQHaHa&pid=Api&P=0&h=180',
+      email: 'mailto:mdtabish.manzer.cs.2022@mitmeerut.ac.in',
+      linkedin: 'https://linkedin.com/in/md-tabish-manzer-18aa7b241',
     },
     {
-      name: 'Rishu',
-      position: 'Designer',
-      image: 'https://tse3.mm.bing.net/th?id=OIP.2TBlFmTmCrCphzvo4qo-gQHaHa&pid=Api&P=0&h=180',
-      instagram: 'https://www.instagram.com',
-      linkedin: 'https://www.linkedin.com',
-      twitter: 'https://www.twitter.com',
+      name: 'Anubhav Raj',
+      position: 'Co-Operative Head',
+      image: 'https://tse4.mm.bing.net/th?id=OIP.UexoP4Tvd6jJ9WzbK6QhfwHaEK&pid=Api&P=0&h=180',
+      email: 'mailto:anubhav.raj.aiml.2022@mitmeerut.ac.in',
+      linkedin: 'https://linkedin.com/in/anubhav-raj-537915258',
     },
     {
-        name: 'Rahul',
-        position: 'CEO',
-        image: 'https://tse3.mm.bing.net/th?id=OIP.2TBlFmTmCrCphzvo4qo-gQHaHa&pid=Api&P=0&h=180',
-        instagram: 'https://www.instagram.com',
-        linkedin: 'https://www.linkedin.com',
-        twitter: 'https://www.twitter.com',
-      },
-      {
-        name: 'Tabish',
-        position: 'CTO',
-        image: 'https://tse3.mm.bing.net/th?id=OIP.2TBlFmTmCrCphzvo4qo-gQHaHa&pid=Api&P=0&h=180',
-        instagram: 'https://www.instagram.com',
-        linkedin: 'https://www.linkedin.com',
-        twitter: 'https://www.twitter.com',
-      },
-      {
-        name: 'Ajay',
-        position: 'Developer',
-        image: 'https://tse3.mm.bing.net/th?id=OIP.2TBlFmTmCrCphzvo4qo-gQHaHa&pid=Api&P=0&h=180',
-        instagram: 'https://www.instagram.com',
-        linkedin: 'https://www.linkedin.com',
-        twitter: 'https://www.twitter.com',
-      },
-      {
-        name: 'Rishu',
-        position: 'Designer',
-        image: 'https://tse3.mm.bing.net/th?id=OIP.2TBlFmTmCrCphzvo4qo-gQHaHa&pid=Api&P=0&h=180',
-        instagram: 'https://www.instagram.com',
-        linkedin: 'https://www.linkedin.com',
-        twitter: 'https://www.twitter.com',
-      },
+      name: 'Rishu Kumar',
+      position: 'Graphic Head',
+      image: 'https://tse4.mm.bing.net/th?id=OIP.hVgph_cSXkho_csoZh6TTwHaJS&pid=Api&P=0&h=180',
+      email: 'mailto:rishu.kumar.aiml.2022@mitmeerut.ac.in',
+      linkedin: 'https://linkedin.com/in/rishu-kumar-496933227',
+    },
+    {
+      name: 'Ajay Kumar',
+      position: 'Web Designing',
+      image: 'https://tse4.mm.bing.net/th?id=OIP.66TlKL0IgZ7ehw7tl95UsAHaGW&pid=Api&P=0&h=180',
+      email: 'mailto:ajay.kumar.aiml.2022@mitmeerut.ac.in',
+      linkedin: 'https://linkedin.com/in/ajay-kumar-429944298',
+    },
+    {
+      name: 'Ayush Raj',
+      position: 'Event Manager',
+      image: 'https://tse4.mm.bing.net/th?id=OIP.JZoat9gfzikncsOgB0MuMQHaIg&pid=Api&P=0&h=180',
+      email: 'mailto:ayush.raj.bpharma.2022@mitmeerut.ac.in',
+      linkedin: 'https://linkedin.com/in/ayush-raj-0453a3264',
+    },
+    {
+      name: 'Reshu Sharma',
+      position: 'Event Manager',
+      image: 'https://tse2.mm.bing.net/th?id=OIP.wDL-A_lXH2fAbz05adCx_AHaEo&pid=Api&P=0&h=180',
+      email: 'mailto:reshu.sharma.cs.2022@mitmeerut.ac.in',
+      linkedin: 'https://linkedin.com/in/reshu-sharma-1bb377259',
+    },
+    {
+      name: 'Nancy',
+      position: 'Event Manager',
+      image: 'https://tse3.mm.bing.net/th?id=OIP.X11iwWjyvhrH6ZXerL3OOwHaIy&pid=Api&P=0&h=180',
+      email: 'mailto:nancy.aiml.2022@mitmeerut.ac.in',
+      linkedin: 'https://linkedin.com/in/nancy-singh-83069b258',
+    },
   ];
 
   return (
@@ -75,7 +74,7 @@ function Team() {
       <div className="absolute inset-0 z-0 bg-cover bg-fixed" style={{ backgroundImage: 'url(https://tse2.mm.bing.net/th?id=OIP.PcX5mOVMNF6P3LafvjBAUQHaE7&pid=Api&P=0&h=180)' }} />
       <div className="relative z-10 container mx-auto px-4">
         <h2 className="text-5xl font-extrabold text-center text-blue-700 mb-12 animate__animated animate__fadeIn">Meet Our Team</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-8">
           {teamMembers.map((member, index) => (
             <div
               key={index}
@@ -88,23 +87,18 @@ function Team() {
                   alt={member.name}
                   className="w-40 h-40 rounded-full object-cover mx-auto mt-6 transform group-hover:scale-110 transition-all duration-300"
                 />
-              
-                
               </div>
               {/* Member Details */}
               <div className="p-6 text-center">
-              <div className="text-red-500 text-xl font-semibold">{member.name}</div>
+                <div className="text-red-500 text-xl font-semibold">{member.name}</div>
                 <h3 className="text-xl font-semibold text-gray-600 mb-3">{member.position}</h3>
                 {/* Social Media Icons */}
                 <div className="flex justify-center gap-6">
-                  <a href={member.instagram} target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-red-500 transition-all duration-300">
-                    <i className="fab fa-instagram text-2xl"></i>
+                  <a href={member.email} target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-red-500 transition-all duration-300">
+                    <i className="fas fa-envelope text-2xl"></i>
                   </a>
                   <a href={member.linkedin} target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-red-500 transition-all duration-300">
                     <i className="fab fa-linkedin-in text-2xl"></i>
-                  </a>
-                  <a href={member.twitter} target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-red-500 transition-all duration-300">
-                    <i className="fab fa-twitter text-2xl"></i>
                   </a>
                 </div>
               </div>
